854dfccfc79274e4a1f35873607dcec120b471df
Amankeldiev Artem <eugenio.jesus@yandex.ru>
2021-01-08 23:48:58 +0300

    1

--- js/yandex.js
+++ js/yandex.js
@@ -1,22 +1,15 @@
 ymaps.ready(init);
 function init() {
-  // Создание карты.
   var myMap = new ymaps.Map("map", {
-    // Координаты центра карты.
-    // Порядок по умолчанию: «широта, долгота».
-    // Чтобы не определять координаты центра карты вручную,
-    // воспользуйтесь инструментом Определение координат.
-    center: [55.760216, 37.628595],
-    // Уровень масштабирования. Допустимые значения:
-    // от 0 (весь мир) до 19.
+    center: [55.759628, 37.611985],
     zoom: 16
   });
 
 
   var myPlacemark = new ymaps.Placemark([55.758463, 37.601079], {}, {
     iconLayout: 'default#image',
-    iconImageHref: 'img/Subtract.png',
-    iconImageSize: [28, 40],
+    iconImageHref: 'img/123.png',
+    iconImageSize: [20, 20],
 });
   myMap.geoObjects.add(myPlacemark);
 }
