efda31195f8fd81b126d78be35c585de97e596c3
Amankeldiev Artem <eugenio.jesus@yandex.ru>
2021-01-09 12:47:49 +0300

    123

--- js/swiper.js
+++ js/swiper.js
@@ -2,10 +2,21 @@ var mySwiper = new Swiper('.swiper-container', {
 
     loop: true,
 
-    // If we need pagination
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
+
+})
+
+
+var mySwiper = new Swiper('.swiper-container_2', {
+
+  loop: true,
+
+  navigation: {
+    nextEl: '.next_2',
+    prevEl: '.prev_2',
+  },
 
 })
