db2448bfa4aa6e2e579b9f10ec8fdb7c7d5c5a1e
Amankeldiev Artem <eugenio.jesus@yandex.ru>
2021-01-11 17:23:46 +0300

    в

--- js/swiper.js
+++ js/swiper.js
@@ -1,22 +1,34 @@
-var mySwiper = new Swiper('.swiper-container', {
+var mySwiper = new Swiper('#swiper_1', {
 
     loop: true,
 
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: '.swiper-button-next_1',
+      prevEl: '.swiper-button-prev_1',
     },
 
 })
 
 
-var mySwiper = new Swiper('.swiper-container_2', {
+var secondSwiper = new Swiper('#swiper_2', {
 
   loop: true,
 
   navigation: {
-    nextEl: '.next_2',
-    prevEl: '.prev_2',
+    nextEl: '.swiper-button-prev_2',
+    prevEl: '.swiper-button-next_2',
+  },
+
+})
+
+
+var lastSwiper = new Swiper('#swiper_3', {
+
+  loop: true,
+
+  navigation: {
+    nextEl: '.swiper-button-prev_3',
+    prevEl: '.swiper-button-next_3',
   },
 
 })
