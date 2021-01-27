43c1c327f022a18abd8b47deaf46fc6baa6219a5
Amankeldiev Artem <eugenio.jesus@yandex.ru>
2021-01-27 23:06:02 +0300

    vb

---
+++ js/scroll.js
@@ -0,0 +1,28 @@
+new SimpleBar(document.getElementById('myscroll_1'), {
+  autoHide: false,
+  scrollbarMaxSize: 28
+});
+
+
+new SimpleBar(document.getElementById('myscroll_2'), {
+  autoHide: false,
+  scrollbarMaxSize: 28
+});
+
+
+new SimpleBar(document.getElementById('myscroll_3'), {
+  autoHide: false,
+  scrollbarMaxSize: 28
+});
+
+
+new SimpleBar(document.getElementById('myscroll_4'), {
+  autoHide: false,
+  scrollbarMaxSize: 28
+});
+
+
+new SimpleBar(document.getElementById('myscroll_5'), {
+  autoHide: false,
+  scrollbarMaxSize: 28
+});
