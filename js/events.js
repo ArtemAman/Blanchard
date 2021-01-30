82af221c98d14f35f0f86f65aa0153f9968461e5
Amankeldiev Artem <eugenio.jesus@yandex.ru>
2021-01-30 10:58:53 +0300

    xc

--- js/events.js
+++ js/events.js
@@ -18,3 +18,77 @@ window.addEventListener('DOMContentLoaded', function () {
   })
 })
 
+
+$(".hero_top_list_dropdown_1").click(function() {
+  $('.scroll_class_1').toggle();
+});
+$(document).on('click', function(e) {
+  if (!$(e.target).closest(".hero_top_list_dropdown_1").length) {
+    $('.scroll_class_1').hide();
+  }
+  e.stopPropagation();
+});
+
+
+
+
+
+$(".hero_top_list_dropdown_2").click(function() {
+  $('.scroll_class_2').toggle();
+});
+$(document).on('click', function(e) {
+  if (!$(e.target).closest(".hero_top_list_dropdown_2").length) {
+    $('.scroll_class_2').hide();
+  }
+  e.stopPropagation();
+});
+
+
+
+
+
+
+
+$(".hero_top_list_dropdown_3").click(function() {
+  $('.scroll_class_3').toggle();
+});
+$(document).on('click', function(e) {
+  if (!$(e.target).closest(".hero_top_list_dropdown_3").length) {
+    $('.scroll_class_3').hide();
+  }
+  e.stopPropagation();
+});
+
+
+
+
+
+
+$(".hero_top_list_dropdown_4").click(function() {
+  $('.scroll_class_4').toggle();
+});
+$(document).on('click', function(e) {
+  if (!$(e.target).closest(".hero_top_list_dropdown_4").length) {
+    $('.scroll_class_4').hide();
+  }
+  e.stopPropagation();
+});
+
+
+
+
+
+
+
+
+$(".hero_top_list_dropdown_5").click(function() {
+  $('.scroll_class_5').toggle();
+});
+$(document).on('click', function(e) {
+  if (!$(e.target).closest(".hero_top_list_dropdown_5").length) {
+    $('.scroll_class_5').hide();
+  }
+  e.stopPropagation();
+});
+
+
