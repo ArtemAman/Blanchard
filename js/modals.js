c45723910feed47d65cfadd83a5cd2e99626bdf7
Amankeldiev Artem <eugenio.jesus@yandex.ru>
2021-03-14 19:49:21 +0300

    modal

--- js/modals.js
+++ js/modals.js
@@ -1,34 +1,42 @@
-const imgs = document.querySelectorAll('.swiper-img');
-const modalOverlay = document.querySelector('.modal-overlay ');
-const modals = document.querySelectorAll('.modal');
+document.addEventListener('DOMContentLoaded', function () {
+	const imgs = document.querySelectorAll('.swiper-img');
+	const modalOverlay = document.querySelector('.modal-overlay');
+	const modals = document.querySelectorAll('.modal');
+	const modalClose = document.querySelector('.modal__exit');
+	const body = document.body;
 
-imgs.forEach((el) => {
-	el.addEventListener('click', (e) => {
-		let path = e.currentTarget.getAttribute('data-path');
-
-		modals.forEach((el) => {
-			el.classList.remove('modal--visible');
+	imgs.forEach((el) => {
+		el.addEventListener('click', (e) => {
+			let path = e.currentTarget.getAttribute('data-path');
+			modals.forEach((el) => {
+				el.classList.remove('modal--visible');
+			});
+			document.querySelector(`[data-target="${path}"]`).classList.add('modal--visible');
+			modalOverlay.classList.add('modal-overlay--visible');
+			let pagePosition = window.scrollY;
+			let paddingOffset = window.innerWidth - document.body.offsetWidth + 'px';
+			body.classList.add('disable-scroll');
+			body.dataset.position = pagePosition;
+			body.style.top = -pagePosition + 'px';
+			document.body.style.paddingRight = paddingOffset;
 		});
-
-		document.querySelector(`[data-target="${path}"]`).classList.add('modal--visible');
-		modalOverlay.classList.add('modal-overlay--visible');
 	});
-});
-
-modalOverlay.addEventListener('click', (e) => {
-
-
-	if (e.target == modalOverlay) {
-		modalOverlay.classList.remove('modal-overlay--visible');
-		modals.forEach((el) => {
-			el.classList.remove('modal--visible');
-		});
-	}
-});
-
-
-
-$(".modal__exit").click(function () {
-  modalOverlay.classList.remove('modal-overlay--visible');
+	modalOverlay.addEventListener('click', (e) => {
+		if (e.target == modalOverlay || e.target == modalClose) {
+			modalOverlay.classList.remove('modal-overlay--visible');
+			modals.forEach((el) => {
+				el.classList.remove('modal--visible');
+			});
+			let pagePosition = parseInt(body.dataset.position, 10);
+			body.style.top = 'auto';
+			body.classList.remove('disable-scroll');
+			window.scroll({
+				top: pagePosition,
+				left: 0
+			});
+			body.removeAttribute('data-position');
+			document.body.style.paddingRight = '';
+		}
+	});
 });
 
